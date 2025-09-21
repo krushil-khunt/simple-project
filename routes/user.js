b5272943e4bfce966c8af4445d25a06eca6dd469
krushil khunt <krushilkh@gmail.com>
2025-09-21 12:38:00 +0530

    Add functionality: post login page

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router=express.Router({mergeParams:true});
 const User=require("../models/user.js");
 const wrapAsycn=require("../utils/wrapAsync.js");
 const passport = require("passport");
+const {saveRedirectUrl}=require("../middleware.js");
 
 router.get("/signup",(req,res)=>{
     res.render("users/singup.ejs");
@@ -34,13 +35,15 @@ router.get("/login",(req,res)=>{
 
 router.post(
     "/login",
+    saveRedirectUrl,
     passport.authenticate("local",{
         failureRedirect: '/login',
         failureFlash:true 
     }),
     async(req,res)=>{
-        req.flash("succes","welcome back to Airban! You are Logged in!");  
-        res.redirect("/listing");  
+        req.flash("succes","welcome back to Airban! You are Logged in!"); 
+        let  redirectUrl=res.locals.redirectUrl || "/listing";
+        res.redirect(redirectUrl);  
     });
 
 router.get("/logout",(req,res,next)=>{
